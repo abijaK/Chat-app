7d23e4205ea04d10e756bc182df1ffa59d568ac1
AbijaK <abijamk@gmail.com>
2022-11-10 10:57:41 +0100

    refactor: update style for buttons

--- client/src/views/Login.jsx
+++ client/src/views/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaRegEnvelope, FaEye, FaLock } from 'react-icons/fa';
+import { FaRegEnvelope, FaEye} from 'react-icons/fa';
 import { MdLockOutline } from "react-icons/md";
 
 function Login() {
@@ -11,14 +11,16 @@ function Login() {
               <h3 className='text-3xl font-bold mb-2'>Goto Sign-up</h3>
                 <div className="border-2 w-14 border-white inline-block mb-2"></div>{/* Barre */}
               <p className='mb-6'>Créez votre compte et connectez-vous pour débuter avec nous !</p>
+              
+              {/* Sign-up Button */}
               <a className='loginBtn rounded-full py-2 px-12 border inline-block font-semibold bg-constancia-blue border-white text-white
-              hover:bg-white hover:text-constancia-blue' 
+              hover:bg-white hover:text-cyan-900 hover:bg-opacity-40' 
                   href='\'>Sign-up</a>
             </div>
-              <div className="text-left text-white font-bold absolute mt-5 ml-8 border-b border-r rounded border-teal-100 px-2"
-                >Design<span className='text-teal-800'>AK</span></div>
+              <div className="text-left text-white font-bold absolute mt-5 ml-8 border-b border-r rounded border-teal-100 
+                px-2"><a href="/" className='outline-none'>Design<span className='text-cyan-900'>AK</span></a></div>
             <div className='w-3/5 py-16 bg-white bg-opacity-40 gap-8 rounded-tr-2xl rounded-bl-2xl flex flex-col items-center'>{/* Sign in section */}
-                <div className='text-3xl text-center text-teal-800 font-bold'><h2>Sign-in</h2>
+                <div className='text-3xl text-center text-cyan-900 font-bold'><h2>Sign-in</h2>
                   <div className="border-2 w-14 border-constancia-blue inline-block"></div>{/* Barre */}
                 </div>
                 <div className="input-item w-80 p-2 gap-2 bg-white rounded flex items-center">
@@ -33,12 +35,14 @@ function Login() {
                 <div className="input-item">
                   <div className="w-80 mb-5">
                     <label htmlFor="remember" className='text-xs flex justify-between'>
-                      <span className='flex text-gray-600'><input type="checkbox" name='remember' className='mr-1' />Remember me</span>
-                    <a href="/" className='text-gray-600'>Forgot Password ?</a>
+                      <span className='flex text-cyan-900'><input type="checkbox" name='remember' className='mr-1' />Remember me</span>
+                    <a href="/" className='text-cyan-900'>Forgot Password ?</a>
                     </label>
                   </div>
-                  <button className='loginBtn w-80 rounded py-2 bg-constancia-blue text-yellow-50 font-semibold'
-                   type="submit">Login</button>
+
+                  {/* Sign-up Button */}
+                  <a href='/' className='loginBtn w-80 rounded py-2 text-yellow-50 font-semibold bg-constancia-blue hover:text-cyan-900 hover:bg-opacity-70'
+                   type="submit">Login</a>
                 </div>
             </div>
           </div>
