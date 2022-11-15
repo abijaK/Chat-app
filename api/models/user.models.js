7ea11c77b85a9565bbb7bce38c8075d8a741fe5b
AbijaK <abijamk@gmail.com>
2022-11-15 14:25:30 +0100

    feat: add function for password encryption

--- api/models/user.models.js
+++ api/models/user.models.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
 const mongooseUniqueValid = require('mongoose-unique-validator');
 
     const userModel = mongoose.Schema({
@@ -18,7 +20,22 @@ const mongooseUniqueValid = require('mongoose-unique-validator');
         { timestamps: true }
 );
 
-    
+    // Compare entered password with password stored in DB
+    userModel.methods.matchPassword = async function(enteredPassword){
+        return await bcrypt.compare(enteredPassword, this.password)
+    }
+
+    // Crypts password before saving
+    userModel.pre("save", async function(next){
+        if (!this.isModified) {
+            next();
+        }
+
+        // Generate a new password crypted
+        const salt = await bcrypt.genSalt(10)
+        this.password = await bcrypt.hash(this.password, salt)
+    })
+
 
 mongoose.plugin(mongooseUniqueValid);
 
