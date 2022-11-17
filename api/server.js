4e6cbbc641e0f2da941810216871895fbab33c43
AbijaK <abijamk@gmail.com>
2022-11-17 12:16:21 +0100

    merge applied and update dependancies on branch main

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
 const PORT = process.env.PORT || 5000;
-const chats = require('./config/data');
+// const chats = require('./config/data');
 const routes = require('./routes/routes');
 const dbConnexion = require('./config/dbConnect');
 const { endPointNotFound, errorHandler } = require('./middlewares/error.middlewares');
@@ -27,20 +27,20 @@ app.options(/.*/,(req, res) => {
 
 app.use(routes);
 
-app.get('/', (req, res) => {
-    res.send('API is running')
-})
+// app.get('/', (req, res) => {
+//     res.send('API is running')
+// })
 
-app.use('/api/user', userRoutes)
+// app.use('/api/user', userRoutes)
 
-app.get('/api/chats', (req,res) => {
-    res.send(chats)
-})
+// app.get('/api/chats', (req,res) => {
+//     res.send(chats)
+// })
 
-app.get('/api/chat/:id', (req, res) => {
-    const oneChat = chats.find((chat) => chat._id === req.params.id)
-    res.send(oneChat);
-})
+// app.get('/api/chat/:id', (req, res) => {
+//     const oneChat = chats.find((chat) => chat._id === req.params.id)
+//     res.send(oneChat);
+// })
 
 app.use(endPointNotFound)
 app.use(errorHandler)
