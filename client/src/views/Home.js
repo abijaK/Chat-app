14839899f5a3c653edbe696d523d04e9d514b2c1
AbijaK <abijamk@gmail.com>
2022-11-27 21:46:15 +0100

    prepare chat page to manage user interaction

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
 // import Login from '../components/Authentication/Login'
-import Signup from '../components/Authentication/Signup'
+import Signup from '../components/auth/Signup'
 
 function Home() {
   return (
